f09ab1651a689ff2014a6d0fdd20fd8f23b4722a
Kajan Muthulingam <kajanm2006@gmail.com>
2024-08-27 23:28:49 +0200

    home sans animation

--- Makenz/src/components/local/home.jsx
+++ Makenz/src/components/local/home.jsx
@@ -33,7 +33,7 @@ export default function Home() {
 
             <section className='h-3/4 flex flex-col items-center justify-evenly mb-9'>
                 <h2
-                    className='text-white text-3xl font-bold underline decoration-custom decoration-8 underline-offset-0'
+                    className='text-white text-2xl font-bold underline decoration-custom decoration-8 underline-offset-0'
                     style={{ textDecorationSkipInk: 'none' }}
                 >
                     Plus qu'une simple coupe
@@ -43,55 +43,60 @@ export default function Home() {
                 <img
                     src={saloon}
                     alt="image"
-                    className='max-w-80 rounded shadow-custom'
+                    className='max-w-72 rounded shadow-custom'
                     style={{ boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)' }}
                 />
 
             </section>
 
 
-            <section className='h-2/4 flex flex-col items-start pl-10 justify-around'>
+            <section className='h-2/4 flex flex-col items-start pl-10 justify-around mb-9'>
                 <h2 className='text-white text-3xl font-bold underline decoration-custom decoration-8 underline-offset-0' style={{ textDecorationSkipInk: 'none' }}>À propos                 </h2>
                 <p className='text-white font-extralight w-4/6 whitespace-break-spaces'>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte.</p>
                 <a href="#" className='rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-opacity-30 ring-gray-300 min-w-48 min-h-10 flex items-center justify-evenly font-extraligth text-sm'><p>En savoir plus</p> <img src={arrowSvg} alt="->" className='min-w-6 max-w-9' /></a>
                 <img src={barber} alt="barber things" className='min-w-56 absolute right-0 opacity-10' />
             </section>
 
-            <section className='h-3/4 grid grid-cols-4 grid-rows-4 gap-4 mb-9 max-w-full'>
-                <div className='col-span-2 row-span-2'>
-                    <img
-                        src={saloon}
-                        alt="image"
-                        className='w-full h-full object-cover rounded shadow-custom'
-                        style={{ boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)' }}
-                    />
-                </div>
-                <div className='col-span-2 row-span-1'>
-                    <img
-                        src={barbing}
-                        alt="image"
-                        className='w-full h-full object-cover rounded shadow-custom'
-                        style={{ boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)' }}
-                    />
-                </div>
-                <div className='col-span-1 row-span-2'>
-                    <img
-                        src={barbing2}
-                        alt="image"
-                        className='w-full h-full object-cover rounded shadow-custom'
-                        style={{ boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)' }}
-                    />
-                </div>
-                <div className='col-span-1 row-span-1'>
-                    <img
-                        src={outils}
-                        alt="image"
-                        className='w-full h-full object-cover rounded shadow-custom'
-                        style={{ boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)' }}
-                    />
+            <section className='h-2/4 flex items-center justify-center'>
+                <div className='h-full grid grid-cols-4 grid-rows-2 gap-4 max-w-96 bg-slate-900'>
+                    <div className='col-span-4 row-span-1'>
+                        <img
+                            src={outils}
+                            alt="image"
+                            className='w-full h-full object-cover' />
+                    </div>
+                    <div className='col-span-2 row-span-1'>
+                        <img
+                            src={saloon}
+                            alt="image"
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+
+                    <div className='col-span-2 row-span-2'>
+                        <img
+                            src={barbing2}
+                            alt="image"
+                            className='w-full h-full object-cover'
+
+                        />
+                    </div>
+
+                    <div className='col-span-2 row-span-1'>
+                        <img
+                            src={barbing}
+                            alt="image"
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
                 </div>
             </section>
 
+            <section className='h-1/4 flex items-center justify-center'>
+                <h1 className='text-white text-5xl font-bold'>
+                    Makenz
+                </h1>
+            </section>
         </>
     );
 }
