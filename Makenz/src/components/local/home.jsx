66ac2f19b2ecacee99f5af0f9192715daa758490
Kajan Muthulingam <kajan.mthln@eduge.ch>
2024-08-28 12:41:12 +0200

    df

--- Makenz/src/components/local/home.jsx
+++ Makenz/src/components/local/home.jsx
@@ -29,20 +29,17 @@ export default function Home() {
 
 
                     <TypingAnimation
-                        className="relative top-16 text-white text-5xl font-bold stroke-white"
+                        className="relative top-16 text-white text-5xl font-bold stroke-white lg:text-8xl"
                         text="Makenz"
                     />
 
-                    <a href="#" className='relative bottom-56 bg-custom text-white w-60 h-10 rounded font-semibold flex items-center justify-center mt-14'>Prendre un rendez-vous</a>
+                    <a href="#" className='relative bottom-56 bg-custom text-white w-60 h-10 rounded font-semibold flex items-center justify-center lg:min-w-96 lg:min-h-16 lg:text-xl'>Prendre un rendez-vous</a>
                 </div>
             </section>
 
-            <section className='h-3/4 flex flex-col items-center justify-evenly mb-9'>
-
-
-
+            <section className='flex flex-col items-center gap-20 justify-evenly mb-9 md:max-h-full'>
                 <h2
-                    className='text-white text-2xl font-bold underline decoration-custom decoration-8 underline-offset-0'
+                    className='text-white text-2xl font-bold underline decoration-custom decoration-8 underline-offset-0 sm:text-4xl lg:text-6xl'
                     style={{ textDecorationSkipInk: 'none' }}
                 >
                     Plus qu'une simple coupe
@@ -52,9 +49,14 @@ export default function Home() {
                 <img
                     src={saloon}
                     alt="image"
-                    className='max-w-72 rounded shadow-custom'
-                    style={{ boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)' }}
+                    className='w-full max-w-xs sm:max-h-max md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded shadow-custom'
+                    style={{
+                        boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)',
+                        height: 'auto', 
+                        objectFit: 'cover' 
+                    }}
                 />
+
 
             </section>
 
@@ -63,11 +65,11 @@ export default function Home() {
                 <h2 className='text-white text-3xl font-bold underline decoration-custom decoration-8 underline-offset-0' style={{ textDecorationSkipInk: 'none' }}>À propos                 </h2>
                 <p className='text-white font-extralight w-4/6 whitespace-break-spaces'>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte.</p>
                 <a href="#" className='rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-opacity-30 ring-gray-300 min-w-48 min-h-10 flex items-center justify-evenly font-extraligth text-sm'><p>En savoir plus</p> <img src={arrowSvg} alt="->" className='min-w-6 max-w-9' /></a>
-                <img src={barber} alt="barber things" className='min-w-56 absolute right-0 opacity-10' />
+                <img src={barber} alt="barber things" className='min-w-56 absolute right-0 opacity-10 -z-10' />
             </section>
 
-            <section className='h-2/4 flex items-center justify-center'>
-                <div className='h-full grid grid-cols-4 grid-rows-2 gap-4 max-w-96 bg-slate-900'>
+            <section className='h-2/4 flex items-center justify-center sm:h-3/5'>
+                <div className='h-full grid grid-cols-4 grid-rows-2 gap-4 max-w-96'>
                     <div className='col-span-4 row-span-1'>
                         <img
                             src={outils}
@@ -101,11 +103,11 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className='h-1/6 flex items-center justify-center'>
+            <section className='h-1/6 flex items-center justify-center sm:h-1/5'>
                 <VelocityScroll
                     text="Makenz"
                     default_velocity={5}
-                    className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-white drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+                    className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-white drop-shadow-sm dark:text-white sm:text-6xl md:leading-[5rem]"
                 />
             </section>
         </>
