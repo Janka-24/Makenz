0a27417ed8a17767f02a1a76be854af1a98184f7
Kajan <kajanm2006@gmail.com>
2024-08-31 14:15:02 +0200

    bento grid bug mais main ok

--- Makenz/src/components/local/header.jsx
+++ Makenz/src/components/local/header.jsx
@@ -2,14 +2,15 @@ import logo from '@/assets/logo.png'
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between bg-black w-full h-16 text-green-600">
-            <div className="w-1/3 h-full flex items-center p-4">
-                <img src={logo} alt="logo" className='w-1/3'/>
+        <header className="flex items-center justify-between bg-black w-full h-16 lg:h-28 text-green-600">
+            <div className="w-1/2 md:w-1/3 h-full flex items-center p-4">
+                <img src={logo} alt="logo" className="max-h-full max-w-full object-contain" />
             </div>
-            <div className="w-1/3 h-full flex items-center justify-end p-4">
-                Burger
+            <div className="w-1/2 md:w-1/3 h-full flex items-center justify-end p-4">
+                <button className="text-sm md:text-base lg:text-lg">
+                    Burger
+                </button>
             </div>
         </header>
     );
 }
-
--- Makenz/src/components/local/home copy.jsx
+++ Makenz/src/components/local/home copy.jsx
@@ -9,13 +9,13 @@ import arrowSvg from '@/assets/arrow-right-short.svg';
 import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 import TypingAnimation from "@/components/magicui/typing-animation";
 
+
 export default function Home() {
     return (
-        <main className='flex flex-col gap-20'>
-
-            <section className='h-screen'>
+        <>
+            <section className='h-full'>
                 <div
-                    className='h-full bg-cover bg-center flex flex-col items-center justify-center'
+                    className='h-full bg-cover bg-center flex flex-col items-center justify-between'
                     style={{
                         backgroundImage: `linear-gradient(to bottom, 
                     rgba(0, 0, 0, 1) 0%,          
@@ -26,16 +26,18 @@ export default function Home() {
                     url(${homeCover})`,
                     }}
                 >
+
+
                     <TypingAnimation
-                        className="text-white text-5xl font-bold stroke-white lg:text-8xl"
+                        className="relative top-16 text-white text-5xl font-bold stroke-white lg:text-8xl"
                         text="Makenz"
                     />
-                    <a href="#" className='bg-custom text-white w-60 h-10 rounded font-semibold flex items-center justify-center lg:min-w-96 lg:min-h-16 lg:text-xl mt-8'>Prendre un rendez-vous</a>
+
+                    <a href="#" className='relative bottom-56 bg-custom text-white w-60 h-10 rounded font-semibold flex items-center justify-center lg:min-w-96 lg:min-h-16 lg:text-xl'>Prendre un rendez-vous</a>
                 </div>
             </section>
 
-
-            <section className='flex flex-col items-center gap-20 justify-evenly p-6'>
+            <section className='flex flex-col items-center gap-20 justify-evenly mb-9 md:max-h-full'>
                 <h2
                     className='text-white text-2xl font-bold underline decoration-custom decoration-8 underline-offset-0 sm:text-4xl lg:text-6xl'
                     style={{ textDecorationSkipInk: 'none' }}
@@ -43,40 +45,42 @@ export default function Home() {
                     Plus qu'une simple coupe
                 </h2>
 
+
                 <img
                     src={saloon}
-                    alt="saloon"
-                    className='w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded shadow-custom'
+                    alt="image"
+                    className='w-full max-w-xs sm:max-h-max md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded shadow-custom'
                     style={{
                         boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)',
                         height: 'auto',
                         objectFit: 'cover'
                     }}
                 />
+
+
             </section>
 
 
-            <section className='flex flex-col items-start pl-6 lg:pl-40 gap-20'>
+            <section className='h-2/4 flex flex-col items-start pl-10 justify-around mb-9 lg:pl-40'>
                 <h2 className='text-white text-3xl font-bold underline decoration-custom decoration-8 underline-offset-0 lg:text-4xl' style={{ textDecorationSkipInk: 'none' }}>À propos                 </h2>
                 <p className='text-white font-extralight w-4/6 whitespace-break-spaces lg:text-xl lg:w-2/6'>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte.</p>
                 <a href="#" className='rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-opacity-30 ring-gray-300 min-w-48 min-h-10 flex items-center justify-evenly font-extraligth text-sm'><p>En savoir plus</p> <img src={arrowSvg} alt="->" className='min-w-6 max-w-9' /></a>
                 <img src={barber} alt="barber things" className='min-w-56 absolute right-0 opacity-10 -z-10' />
             </section>
 
-
-            <section className="flex items-center justify-center bg-slate-200 py-8">
-                <div className="grid grid-cols-4 grid-row-2 gap-4 max-w-4xl">
+            <section className='items-center justify-center bg-slate-300 hidden lg:flex'>
+                <div className='grid grid-cols-4 grid-rows-2 gap-4 max-w-4xl mx-auto'>
                     <div className='col-span-4 row-span-1'>
                         <img
                             src={outils}
                             alt="image"
-                            className='object-cover' />
+                            className='w-full h-full object-cover' />
                     </div>
                     <div className='col-span-2 row-span-1'>
                         <img
                             src={saloon}
                             alt="image"
-                            className='object-cover'
+                            className='w-full h-full object-cover'
                         />
                     </div>
 
@@ -84,7 +88,7 @@ export default function Home() {
                         <img
                             src={barbing2}
                             alt="image"
-                            className='object-cover'
+                            className='w-full h-full object-cover'
 
                         />
                     </div>
@@ -93,7 +97,7 @@ export default function Home() {
                         <img
                             src={barbing}
                             alt="image"
-                            className='object-cover'
+                            className='w-full h-full object-cover'
                         />
                     </div>
                 </div>
@@ -101,13 +105,51 @@ export default function Home() {
 
 
 
-            <section className='h-1/6 flex items-center justify-center sm:h-1/5 py-12 '>
+
+
+            <section className='h-2/4 flex items-center justify-center sm:h-3/5 lg:hidden'>
+                <div className='h-full grid grid-cols-4 grid-rows-2 gap-4 max-w-96'>
+                    <div className='col-span-4 row-span-1'>
+                        <img
+                            src={outils}
+                            alt="image"
+                            className='w-full h-full object-cover' />
+                    </div>
+                    <div className='col-span-2 row-span-1'>
+                        <img
+                            src={saloon}
+                            alt="image"
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+
+                    <div className='col-span-2 row-span-2'>
+                        <img
+                            src={barbing2}
+                            alt="image"
+                            className='w-full h-full object-cover'
+
+                        />
+                    </div>
+
+                    <div className='col-span-2 row-span-1'>
+                        <img
+                            src={barbing}
+                            alt="image"
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section className='h-1/6 flex items-center justify-center sm:h-1/5'>
                 <VelocityScroll
                     text="Makenz"
                     default_velocity={5}
                     className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-white drop-shadow-sm dark:text-white sm:text-6xl md:leading-[5rem]"
                 />
             </section>
-        </main>
+        </>
     );
 }
+
