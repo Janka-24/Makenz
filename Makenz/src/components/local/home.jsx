793595497e9673c3c4046cfe4c2e41bb697563c5
Kajan <kajanm2006@gmail.com>
2024-08-31 12:27:12 +0200

    bento grid bug

--- Makenz/src/components/local/home.jsx
+++ Makenz/src/components/local/home.jsx
@@ -52,8 +52,8 @@ export default function Home() {
                     className='w-full max-w-xs sm:max-h-max md:max-w-lg lg:max-w-xl xl:max-w-2xl rounded shadow-custom'
                     style={{
                         boxShadow: '0 10px 90px rgba(113, 72, 10, 0.4)',
-                        height: 'auto', 
-                        objectFit: 'cover' 
+                        height: 'auto',
+                        objectFit: 'cover'
                     }}
                 />
 
@@ -61,14 +61,53 @@ export default function Home() {
             </section>
 
 
-            <section className='h-2/4 flex flex-col items-start pl-10 justify-around mb-9'>
-                <h2 className='text-white text-3xl font-bold underline decoration-custom decoration-8 underline-offset-0' style={{ textDecorationSkipInk: 'none' }}>À propos                 </h2>
-                <p className='text-white font-extralight w-4/6 whitespace-break-spaces'>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte.</p>
+            <section className='h-2/4 flex flex-col items-start pl-10 justify-around mb-9 lg:pl-40'>
+                <h2 className='text-white text-3xl font-bold underline decoration-custom decoration-8 underline-offset-0 lg:text-4xl' style={{ textDecorationSkipInk: 'none' }}>À propos                 </h2>
+                <p className='text-white font-extralight w-4/6 whitespace-break-spaces lg:text-xl lg:w-2/6'>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte.</p>
                 <a href="#" className='rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-opacity-30 ring-gray-300 min-w-48 min-h-10 flex items-center justify-evenly font-extraligth text-sm'><p>En savoir plus</p> <img src={arrowSvg} alt="->" className='min-w-6 max-w-9' /></a>
                 <img src={barber} alt="barber things" className='min-w-56 absolute right-0 opacity-10 -z-10' />
             </section>
 
-            <section className='h-2/4 flex items-center justify-center sm:h-3/5'>
+            <section className='flex items-center justify-center bg-slate-300'>
+                <div className='grid grid-cols-4 grid-rows-2 gap-4 max-w-4xl mx-auto'>
+                    <div className='col-span-4 row-span-1'>
+                        <img
+                            src={outils}
+                            alt="image"
+                            className='w-full h-full object-cover' />
+                    </div>
+                    <div className='col-span-2 row-span-1'>
+                        <img
+                            src={saloon}
+                            alt="image"
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+
+                    <div className='col-span-2 row-span-2'>
+                        <img
+                            src={barbing2}
+                            alt="image"
+                            className='w-full h-full object-cover'
+
+                        />
+                    </div>
+
+                    <div className='col-span-2 row-span-1'>
+                        <img
+                            src={barbing}
+                            alt="image"
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+            <section className='h-2/4 flex items-center justify-center sm:h-3/5 lg:hidden'>
                 <div className='h-full grid grid-cols-4 grid-rows-2 gap-4 max-w-96'>
                     <div className='col-span-4 row-span-1'>
                         <img
